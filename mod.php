4030deef9d43a83ced5b10464264511d412e02dc
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2019-06-02 10:26:24 +0900

    あまりで計算するライブラリ
    powermの実装を改善しました。こちらのほうが早そう。1行少ないのはよいことだ。

--- mod.php
+++ mod.php
@@ -25,7 +25,6 @@ function divm($a, $b){
 
 function powerm($a, $b){
     if($b == 0)return 1;
-    if($b == 1)return $a;
     if($b & 1)return (powerm($a, $b>>1)**2 % MOD) * $a % MOD;
     return powerm($a, $b>>1)**2 % MOD;
 }
