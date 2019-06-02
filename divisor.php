136aba6910b7a9a2ae8f0efe03bd0be40cbee11e
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2019-06-02 10:08:30 +0900

    約数列挙、計算量はO(N^0.5)

---
+++ divisor.php
@@ -0,0 +1,13 @@
+<?php
+
+//約数
+function divisor($n){
+    for($i = ceil(sqrt($n)); $i >= 1; $i--){
+        if($n % $i == 0){
+            $f[] = $i;
+            $f[] = $n / $i;
+        }
+    }
+    sort($f);
+    return array_unique($f);
+}
