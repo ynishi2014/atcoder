19fb4110771c040a8494ddc2d158bc6af84d83dc
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2019-06-09 12:32:08 +0900

    SPLのデータ構造／特に優先度付キュー

---
+++ SPL_stack_queue_priorityQueue.php
@@ -0,0 +1,58 @@
+<?php
+//Stack -- スタック --配列を使えば十分ではある
+$s = new SplStack();
+$s->push(1);
+$s->push(2);
+echo $s->pop();
+echo $s->pop();
+echo "\n";
+//12
+
+//Queue -- キュー
+$q = new SplQueue();
+$q->enqueue(1);
+$q->enqueue(2);
+echo $q->dequeue();
+echo $q->dequeue();
+echo "\n";
+//21
+
+//https://php.net/manual/ja/class.splpriorityqueue.php
+//PriorityQueue -- 値を取り出す
+$pq = new SplPriorityQueue();
+$pq->insert("One",1);
+$pq->insert("Three",3);
+$pq->insert("Two",2);
+echo $pq->extract(),"\n";
+echo $pq->extract(),"\n";
+echo $pq->extract(),"\n";
+//Three
+//Two
+//One
+
+//PriorityQueue -- 優先度を取り出す
+$pq = new SplPriorityQueue();
+$pq->setExtractFlags(SplPriorityQueue::EXTR_PRIORITY);
+$pq->insert("One",1);
+$pq->insert("Three",3);
+$pq->insert("Two",2);
+echo $pq->extract(),"\n";
+echo $pq->extract(),"\n";
+echo $pq->extract(),"\n";
+//3
+//2
+//1
+
+//PriorityQueue -- 両方取り出す
+$pq = new SplPriorityQueue();
+$pq->setExtractFlags(SplPriorityQueue::EXTR_BOTH);
+$pq->insert("One",1);
+$pq->insert("Three",3);
+$pq->insert("Two",2);
+echo json_encode($pq->extract()),"\n";
+echo json_encode($pq->extract()),"\n";
+echo json_encode($pq->extract()),"\n";
+//{"data":"Three","priority":3}
+//{"data":"Two","priority":2}
+//{"data":"One","priority":1}
+
