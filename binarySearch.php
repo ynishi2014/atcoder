d9646d70494a6ce6cc0dc4af5e6fc55addc38ca0
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2019-12-31 11:20:18 +0900

    値を返すタイプの二分探索は使いそうもないので削除

--- binarySearch.php
+++ binarySearch.php
@@ -1,33 +1,6 @@
 <?php
 
-//二分探索　target以下で最大の値を返す
-function bsl($nums, $target) {
-    $size = count($nums);
-    $min = 0; 
-    $max = $size - 1;
-    while(true){
-        $center = floor(($min + $max) / 2);
-        $centerValue = $nums[$center];
-        if($target == $centerValue){
-            return $centerValue;
-        }elseif($target > $centerValue){
-            $min = $center + 1;
-        }else{
-            $max = $center;
-        }
-        if($min == $max){
-            $center = floor(($min + $max) / 2);
-            $centerValue = $nums[$center];
-            if($centerValue == $target){
-                return $nums[$center];
-            }elseif($centerValue < $target){
-                return $nums[$center];
-            }else{
-                return $nums[$center-1] ?? -10**18;
-            }
-        }
-    }
-}
+
 //二分探索　target以下で最大の値の場所を返す
 function bsl($nums, $target) {
     $size = count($nums);
@@ -57,37 +30,8 @@ function bsl($nums, $target) {
     }
 }
 
-//二分探索　target以上で最小の値を返す
-function bsr($nums, $target) {
-    $size = count($nums);
-    $min = 0; 
-    $max = $size - 1;
-    while(true){
-        $center = floor(($min + $max) / 2);
-        $centerValue = $nums[$center];
-        if($target == $centerValue){
-            return $centerValue;
-        }elseif($target > $centerValue){
-            $min = $center + 1;
-        }else{
-            $max = $center;
-        }
-        if($min == $max){
-            $center = floor(($min + $max) / 2);
-            $centerValue = $nums[$center];
-            if($centerValue == $target){
-                return $nums[$center];
-            }elseif($centerValue < $target){
-                return $nums[$center+1] ?? 10**18;
-            }else{
-                return $nums[$center];
-            }
-        }
-    }
-}
-
 //二分探索　要素の場所を返す　存在しなければ-1を返す
-function bs($nums, $target) {
+function bsr($nums, $target) {
     $size = count($nums);
     $min = 0; 
     $max = $size - 1;
