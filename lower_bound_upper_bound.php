d86aab046d94e541bab49cfaad848d90fc983789
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2020-01-12 18:06:12 +0900

    lower_boundとupper_boundを追加

---
+++ lower_bound_upper_bound.php
@@ -0,0 +1,39 @@
+<?php
+$array = [1,2,2,3,4];
+$key = 2;
+echo lower_bound($array, $key),"\n";
+echo upper_bound($array, $key),"\n";
+echo upper_bound($array, $key)-lower_bound($array, $key),"\n";
+
+/**
+ * $key以上になる一番左のIndexを返す
+ */
+function lower_bound($array, $key){
+  $min = -1;
+  $max = count($array);
+  while($max - $min > 1){
+    $c = intdiv($min + $max, 2);
+    if($array[$c] >= $key){
+      $max = $c;
+    }else{
+      $min = $c;
+    }
+  }
+  return $max;
+}
+/**
+ * $keyを超える一番左のIndexを返す 
+ */
+function upper_bound($array, $key){
+  $min = -1;
+  $max = count($array);
+  while($max - $min > 1){
+    $c = intdiv($min + $max, 2);
+    if($array[$c] > $key){
+      $max = $c;
+    }else{
+      $min = $c;
+    }
+  }
+  return $max;
+}
