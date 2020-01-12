44de7a89675099863bb40e53356903eba65fac70
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2020-01-12 17:49:46 +0900

    説明を追加/getSegLenは同じ文字の継続数を返す/シンプルだけどバグらせやすいやつ

--- Str.php
+++ Str.php
@@ -4,11 +4,19 @@ $str = new Str($s);
 
 print_r($str->getSegLen());
 
+/**
+ * 文字列に関する処理をまとめる
+ */
 class Str{
   public $str = false;
   function __construct($str){
     $this->str = $str;
   }
+  /**
+   * 同じ文字の継続数を返す
+   * in: "aabbccc"
+   * out: [2,2,3]
+   */
   function getSegLen(){
     $c = 1;
     for($i = 0, $I = strlen($this->str) - 1; $i < $I; $i++){
