60256d09fa3f440b98cdceec05d9f34682504033
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2020-01-23 22:23:48 +0900

    lcs:定数倍高速化　lcstr:結果が決まってから辿ることで抜本的な高速化

--- lcs.php
+++ lcs.php
@@ -1,37 +1,56 @@
 <?php
-//最長共通部分列問題 
-$str1 = "aaaaaaa";
-$str2 = "abcdabc";
-
-echo lcs($str1, $str2);
-function lcs($str1, $str2){
-    for($i = 0; $i < strlen($str1); $i++){
-        for($j = 0; $j < strlen($str2); $j++){
-            if($str1[$i] == $str2[$j]){
-                $dp[$i][$j] = ($dp[$i-1][$j-1]??0) + 1;
-            }else{
-                $dp[$i][$j] = max($dp[$i-1][$j]??0, $dp[$i][$j-1]??0);
-            }
-        }
-    }
-    return $dp[strlen($str1) - 1][strlen($str2) - 1];
-}
+//https://atcoder.jp/contests/dp/submissions/9687959
+$a = str();
+$b = str();
+ 
+echo lcstr($a, $b);
 
 function lcstr($str1, $str2){
+    global $dp,$dpf;
+    lcs($str1, $str2);
+    $i = strlen($str1) - 1;
+    $j = strlen($str2) - 1;
+    $ret = '';
+    while($i >= 0 && $j >= 0){
+        switch($dpf[$i][$j]){
+            case 1:
+                $ret .= $str1[$i];
+                $i--;$j--;
+                break;
+            case 2:
+                $i--;
+                break;
+            case 3:
+                $j--;
+                break;
+        }
+    }
+    return strrev($ret);
+}
+
+function lcs($str1, $str2){
+    global $dp,$dpf;
     $l1 = strlen($str1);
     $l2 = strlen($str2);
+    $dp = array_fill(-1, $l1+1, array_fill(-1, $l2+1, 0));
     for($i = 0; $i < $l1; $i++){
+        $dpb = $dp[$i-1];
+        $dpi = &$dp[$i];
+        $si = $str1[$i];
+        $dpfi = [];
         for($j = 0; $j < $l2; $j++){
-            if($str1[$i] == $str2[$j]){
-                $dp[$i][$j] = ($dp[$i-1][$j-1]??'') . $str1[$i];
+            if($si == $str2[$j]){
+                $dpi[$j] = $dpb[$j-1] + 1;
+                $dpfi[$j] = 1;//斜めからきた
+            }elseif($dpb[$j] > $dpi[$j-1]){
+                $dpi[$j] = $dpb[$j];
+                $dpfi[$j] = 2;//上からきた
             }else{
-                if(strlen($dp[$i-1][$j]??'') > strlen($dp[$i][$j-1]??'')){
-                    $dp[$i][$j] = $dp[$i-1][$j]??'';
-                }else{
-                    $dp[$i][$j] = $dp[$i][$j-1]??'';
-                }
+                $dpi[$j] = $dpi[$j-1];
+                $dpfi[$j] = 3;//左からきた
             }
         }
+        $dpf[] = $dpfi;
     }
     return $dp[$l1 - 1][$l2 - 1];
 }
