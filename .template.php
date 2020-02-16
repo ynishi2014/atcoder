5d3705bef56e6b398d3a946acfd9d2f0506b7b49
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2020-02-16 14:42:27 +0900

    Add zaatsu

--- .template.php
+++ .template.php
@@ -50,3 +50,17 @@ function isCleanArray($array){
     }
     return $clean;
 }
+/**
+ * 座圧対象の配列を渡すと以下の値を返す
+ * ・圧縮された配列
+ * ・復元用のMap
+ * ・圧縮用のMap
+ **/
+function atsu($array){
+    $a = array_flip($array);
+    $fuku = array_flip($a);
+    sort($fuku);
+    $atsu = array_flip($fuku);
+    foreach($array as $i => $val)$array[$i] = $atsu[$val];
+    return [$array, $fuku, $atsu];
+}
