f2a80b29a6e0f3ee05cb84ab7c9842a7705028e0
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2020-03-27 14:44:57 +0900

    ダイクストラ／2割弱高速化
    デフォルトではFrom記憶をコメントアウト（２～３％程度高速化）
    変数参照の最適化（15％程度高速化）
    graphをGに変更（他のライブラリに合わせる）

--- dijkstr.php
+++ dijkstr.php
@@ -1,21 +1,4 @@
 <?php
-list($n, $m) = array_map("intval", explode(" ", trim(fgets(STDIN))));
-$di = new Di($n);
-for($i = 0; $i < $m; $i++){
-  list($from, $to, $d) = array_map("intval", explode(" ", trim(fgets(STDIN))));
-  $di->connect($from, $to, $d);
-}
-for($i = 2; $i <= $n; $i++){
-  $di->connect($i, $i - 1, 0);
-}
-$di->solve(1);
-$distance = $di->distance[$n];
-if($distance == $di->inf){
-  echo -1;
-}else{
-  echo $distance;
-}
-
 /**
  * Dijkstra
  * Attention: 1-origin
@@ -23,33 +6,38 @@ if($distance == $di->inf){
 class Di{
   public $pq;
   public $distance;
-  public $graph;
+  public $G;
   public $from;
   public $inf = 10**18;
   function __construct($n) {
-    $this->pq = new SplPriorityQueue();
     for($i = 1; $i <= $n; $i++){
       $this->distance[$i] = $this->inf;
-      $this->from[$i] = 0;
+      //$this->from[$i] = 0;
     }
   }
   function connect($from, $to, $cost){
     $this->graph[$from][] = [$to, $cost];
   }
   function solve($from){
-    $this->pq->insert($from, 0);
-    $this->distance[$from] = 0;
-    while($this->pq->count()){
-      $f = $this->pq->extract();
-      if(!isset($this->graph[$f]))continue;
-      foreach($this->graph[$f] as list($t, $dist)){
-        $new = $this->distance[$f] + $dist;
-        if($this->distance[$t] > $new){
-          $this->distance[$t] = $new;
-          $this->from[$t] = $f;
-          $this->pq->insert($t, -$new);
+    $pq = new SplPriorityQueue();
+    $distance = $this->distance;
+    $tfrom = $this->from;
+    $G = $this->G;
+    $pq->insert($from, 0);
+    $distance[$from] = 0;
+    while($pq->count()){
+      $f = $pq->extract();
+      if(!isset($G[$f]))continue;
+      foreach($G[$f] as list($t, $dist)){
+        $new = $distance[$f] + $dist;
+        if($distance[$t] > $new){
+          $distance[$t] = $new;
+          //$tfrom[$t] = $f;
+          $pq->insert($t, -$new);
         }
       }
     }
+    $this->distance = $distance;
+    //$this->from = $tfrom;
   }
 }
