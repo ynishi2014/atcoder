cc99e01567c313098ee6e2f77515b44aa37c5f69
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2020-04-24 12:59:33 +0900

    最小値を0ではなくPHP_INT_MINに変更／負の値を処理できるようにする

--- LIS.php
+++ LIS.php
@@ -2,7 +2,7 @@
 
 function lis($A){
     $dp = array_fill(0, count($A)+1, PHP_INT_MAX);
-    $dp[0] = 0;
+    $dp[0] = PHP_INT_MIN;
     $longest = 0;
     foreach($A as $a){
         $b = lower_bound($dp, $a);
