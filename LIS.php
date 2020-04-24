ec9c89c6fe36aab28eb6bc6b91fa5f14cb47990d
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2020-04-24 13:11:25 +0900

    狭義・広義／減少・増加の切り替えに対応させる

--- LIS.php
+++ LIS.php
@@ -1,11 +1,19 @@
 <?php
+echo lis([1,2,3,4,4,3], false, true);
 
-function lis($A){
+/**
+ * 単調増加列
+ * @param $A 配列
+ * @param $kougi true：広義 false：狭義
+ * @param $inv true：減少列
+ **/
+function lis($A, $kougi = false, $inv = false){
+    if($inv)foreach($A as $i => $a)$A[$i] = -$a;
     $dp = array_fill(0, count($A)+1, PHP_INT_MAX);
     $dp[0] = PHP_INT_MIN;
     $longest = 0;
     foreach($A as $a){
-        $b = lower_bound($dp, $a);
+        $b = lower_bound($dp, $a+$kougi);
         $dp[$b] = $a;
         $longest = max($longest, $b);
     }
