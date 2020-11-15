e33bdbdcd243f0e21c8e77c8496b719d22355197
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2020-11-15 17:35:35 +0900

    トポロジカルソート -- グラフを渡すように変更／Nextが存在しない場合にエラーにならないよう修正

--- tsort.php
+++ tsort.php
@@ -1,15 +1,17 @@
 <?php
 
-function tsort(){
-    global $N, $ans;
-    for($i = 1; $i <= $N; $i++)visit($i);
+function tsort($G){
+    global $__G__, $ans;
+    $__G__ = $G;
+    $ans = [];
+    for($i = 0; $i < count($G); $i++)visit($i);
     return $ans;
 }
 function visit($i){
-    global $G, $visited, $ans;
+    global $__G__, $visited, $ans;
     if(!isset($visited[$i])){
         $visited[$i] = true;
-        foreach($G[$i] as $next){
+        foreach($__G__[$i]??[] as $next){
             visit($next);
         }
         $ans[] = $i;
