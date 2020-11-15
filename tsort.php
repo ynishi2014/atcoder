4aa9ef5c7548c3052a99339fffc1fb0b222c473f
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2020-11-15 20:03:49 +0900

    クラス化／結果が逆順になる不具合を修正

--- tsort.php
+++ tsort.php
@@ -1,19 +1,21 @@
 <?php
 
-function tsort($G){
-    global $__G__, $ans;
-    $__G__ = $G;
-    $ans = [];
-    for($i = 0; $i < count($G); $i++)visit($i);
-    return $ans;
-}
-function visit($i){
-    global $__G__, $visited, $ans;
-    if(!isset($visited[$i])){
-        $visited[$i] = true;
-        foreach($__G__[$i]??[] as $next){
-            visit($next);
+class TSORT{
+    static function sort($G){
+        global $__G__, $ans;
+        $__G__ = $G;
+        $ans = [];
+        for($i = 1; $i <= count($G); $i++)TSORT::visit($i);
+        return array_reverse($ans);
+    }
+    static function visit($i){
+        global $__G__, $visited, $ans;
+        if(!isset($visited[$i])){
+            $visited[$i] = true;
+            foreach($__G__[$i]??[] as $next){
+                TSORT::visit($next);
+            }
+            $ans[] = $i;
         }
-        $ans[] = $i;
     }
 }
