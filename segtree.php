8676de1949cb93454448821e8606ae0202d7c3a8
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2021-01-16 20:16:39 +0900

    セグ木ごとに異なるゼロ元と演算子を使えるように改良

--- segtree.php
+++ segtree.php
@@ -1,19 +1,23 @@
 <?php
-function op($a, $b){return $a | $b;}
-$zero = 0;
-
-//$seg = new SegTree($array);
+//$segMax = new SegTree($P, "max", 0);
+//$segMin = new SegTree($P, "min", 10**10);
+//$segMax = new SegTree($P, function($a, $b){return max($a, $b);}, 0);
+//$segMin = new SegTree($P, function($a, $b){return min($a, $b);}, 10**10);
 
 class SegTree{
   public $tree;
   public $bits;
-  function __construct($n){
+  public $zero;
+  public $op;
+  function __construct($n, callable $op = null, $zero = 0){
+    $this->op = $op;
+    $this->zero = $zero;
     if(is_array($n)){
       $array = $n;
       $n = count($array);
       $this->bits = max(1, ceil(log($n+1)/log(2)));
       for($i = $n, $I = 2**$this->bits; $i < $I; ++$i){
-        $array[] = $GLOBALS['zero'];
+        $array[] = $zero;
       }
       $this->tree[0] = $array;
       $this->rollupAll();
@@ -25,33 +29,36 @@ class SegTree{
     }
   }
   function rollupAll(){
+    $op = $this->op;
     for($i = 1; $i < $this->bits; ++$i){
       $treei = &$this->tree[$i];
       $treeim = $this->tree[$i-1];
       for($j = 0, $J = 2**($this->bits - $i + 1); $j < $J; $j+=2){
-        $treei[] = op($treeim[$j], $treeim[$j+1]);
+        $treei[] = $op($treeim[$j], $treeim[$j+1]);
       }
     }
   }
   function set($i, $amount){
     $this->tree[0][$i] = $amount;
+    $op = $this->op;
     for($j = 1, $J = $this->bits; $j < $J; ++$j){
-      $this->tree[$j][$i>>$j] = op($this->tree[$j-1][$i>>$j<<1], $this->tree[$j-1][($i>>$j<<1) + 1]);
+      $this->tree[$j][$i>>$j] = $op($this->tree[$j-1][$i>>$j<<1], $this->tree[$j-1][($i>>$j<<1) + 1]);
     }
   }
   function get($f, $t){
-    $sum = $GLOBALS['zero'];
+    $sum = $this->zero;
     $tree = $this->tree;
     $bits = $this->bits;
+    $op = $this->op;
     $i = 0;
     while($f!=$t){
       $treei = $tree[$i];
       if(($f >> $i) & 1 == 1){
-        $sum = op($sum, $treei[$f>>$i]);
+        $sum = $op($sum, $treei[$f>>$i]);
         $f+=1<<$i;
       }
       if(($t >> $i) & 1 == 1){
-        $sum = op($sum, $treei[($t>>$i)-1]);
+        $sum = $op($sum, $treei[($t>>$i)-1]);
         $t-=1<<$i;
       }
       ++$i;
