96d0e4848297e84d85696e090ad35a0f4307002f
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2021-01-31 10:13:14 +0900

    1.5倍ほど高速化
    https://atcoder.jp/contests/abc190/submissions/19830490

--- BIT.php
+++ BIT.php
@@ -8,16 +8,21 @@ class BIT{
         $this->tree = array_fill(0, $N+1, 0);
         $this->N = $N;
     }
-    public function between($from, $to){//閉区間
-        return $this->sum($to) - $this->sum($from-1);
-    }
     public function sum($i){
-        if($i == 0)return 0;
-        return $this->tree[$i] + $this->sum($i-($i&-$i));
+        $sum = 0;
+        $tree = $this->tree;
+        while($i){
+            $sum += $tree[$i];
+            $i -= $i&-$i;
+        }
+        return $sum;
     }
     public function add($i, $x){
-        if($i > $this->N)return;
-        $this->tree[$i] += $x;
-        $this->add($i+($i&-$i), $x);
+        $N = $this->N;
+        $tree = &$this->tree;
+        while($i <= $N){
+            $tree[$i] += $x;
+            $i += $i&-$i;
+        }
     }
 }
