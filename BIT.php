e4956859f07ea1ab71fd4ea21089b52086bdc38a
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2022-05-15 21:00:32 +0900

    BIT: nth関数を高速化　

--- BIT.php
+++ BIT.php
@@ -7,6 +7,8 @@ class BIT{
     public function __construct($N){
         $this->tree = array_fill(0, $N+1, 0);
         $this->N = $N;
+        $bits = sprintf("%b", $N);
+        $this->d = 2**(strlen($bits)-1);
     }
     public function sum($i){
         $sum = 0;
@@ -20,18 +22,18 @@ class BIT{
     public function sumBetween($i, $j){
         return $this->sum($j) - $this->sum($i-1);
     }
-    public function nth($x){ // 1-origin O(logN * logN) // TODO:高速化
-        $ng = 0;
-        $ok = $this->N;
-        while($ok - $ng > 1){
-            $mid = ($ok+$ng)>>1;
-            if($this->sum($mid)>=$x){
-                $ok = $mid;
-            }else{
-                $ng = $mid;
-            }
-        }
-        return $ok;
+    public function nth($x){
+        $ans = 1;
+        $d = $this->d;
+        $tree = $this->tree;
+        do{
+          if($tree[$ans+$d]<$x){
+            $x-=$tree[$ans+$d];
+            $ans+=$d;
+          }
+          $d>>=1;
+        }while($d);
+        return $ans;
     }
     public function set($i, $x){
         $old = $this->sumBetween($i, $i);
