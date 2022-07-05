e2ef96ade06b98417d868bd5533d2d8d8c0a6cba
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2022-07-05 16:57:04 +0900

    Update popcount.php

--- popcount.php
+++ popcount.php
@@ -3,6 +3,9 @@
  * 1のビットの数を高速にカウントします
  */
 function popcount($x){
+    if(function_exists("gmp_popcount")){
+        return gmp_popcount($x);
+    }
     $con = 0;
     while ($x) {
         $x &= $x-1;
