cca6ad869779a64fa9f3079b35ecd65bac9ae847
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2022-07-18 13:18:00 +0900

    BIT/nth関数が壊れていたので修正／max, min, sumAllを追加

--- BIT.php
+++ BIT.php
@@ -5,11 +5,14 @@ class BIT{
     public $tree;
     public $N;
     public function __construct($N){
-        $this->tree = array_fill(0, $N+1, 0);
-        $this->N = $N;
         $bits = sprintf("%b", $N);
         $this->d = 2**(strlen($bits)-1);
+        $this->tree = array_fill(0, $N+1, 0);
+        $this->N = $N;
     }
+    public function max(){return $this->nth($this->sum($this->N));}
+    public function min(){return $this->nth(1);}
+    public function sumAll(){return $this->sum($this->N);}
     public function sum($i){
         $sum = 0;
         $tree = $this->tree;
@@ -22,18 +25,18 @@ class BIT{
     public function sumBetween($i, $j){
         return $this->sum($j) - $this->sum($i-1);
     }
-    public function nth($x){
-        $ans = 1;
-        $d = $this->d;
+    public function nth(int $w){
+        if($w <= 0)return 0;
+        $x = 0;
+        $N = $this->N;
         $tree = $this->tree;
-        do{
-          if($tree[$ans+$d]<$x){
-            $x-=$tree[$ans+$d];
-            $ans+=$d;
-          }
-          $d>>=1;
-        }while($d);
-        return $ans;
+        for($k = $this->d; $k>0; $k>>=1){
+            if($x+$k <= $N && $tree[$x+$k] < $w){
+                $w-=$tree[$x+$k];
+                $x+=$k;
+            }
+        }
+        return $x+1;
     }
     public function set($i, $x){
         $old = $this->sumBetween($i, $i);
