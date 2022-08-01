6e06e192cb541c7ea369d623750e620f6dac64f2
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2022-08-01 19:57:34 +0900

    change indent from 4 to 2

--- nCm_quick.php
+++ nCm_quick.php
@@ -9,18 +9,18 @@ define("MOD", 10**9+7);
  */
 
 function prepareMod($n){
-    global $f, $invf;
-    $f[0] = $invf[0] = $invf[1] = 1;
-    for($i = 1; $i <= $n; $i++)$f[$i] = $f[$i-1]*$i%MOD;
-    for($i = 2; $i <= $n; $i++)$invf[$i] = (MOD - intdiv(MOD, $i))*$invf[MOD%$i]%MOD;
-    for($i = 2; $i <= $n; $i++)$invf[$i] = $invf[$i]*$invf[$i-1]%MOD;
+  global $f, $invf;
+  $f[0] = $invf[0] = $invf[1] = 1;
+  for($i = 1; $i <= $n; $i++)$f[$i] = $f[$i-1]*$i%MOD;
+  for($i = 2; $i <= $n; $i++)$invf[$i] = (MOD - intdiv(MOD, $i))*$invf[MOD%$i]%MOD;
+  for($i = 2; $i <= $n; $i++)$invf[$i] = $invf[$i]*$invf[$i-1]%MOD;
 }
 function nCm($n, $m){
-    global $f, $invf;
-    if($n < 0 || $n - $m < 0 || $m < 0)return 0;
-    return $f[$n] * $invf[$n-$m] % MOD * $invf[$m] % MOD;
+  global $f, $invf;
+  if($n < 0 || $n - $m < 0 || $m < 0)return 0;
+  return $f[$n] * $invf[$n-$m] % MOD * $invf[$m] % MOD;
 }
 function nHm($n, $m){
-    global $f, $invf;
-    return nCm($n+$m-1, $m);
+  global $f, $invf;
+  return nCm($n+$m-1, $m);
 }
